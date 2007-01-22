8a700420eb16f0101f826c18bb001cafaaf3436d
ajpoland@iupui.edu <ajpoland@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-01-22 21:48:09 +0000

    update SakaiSecurityTest to add isAdminToolsUser function
    git-svn-id: https://source.sakaiproject.org/svn/oncourse/trunk/src/authz@20553 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- authz-impl/impl/src/java/org/sakaiproject/authz/impl/SakaiSecurityTest.java
+++ authz-impl/impl/src/java/org/sakaiproject/authz/impl/SakaiSecurityTest.java
@@ -77,7 +77,7 @@ public class SakaiSecurityTest extends SakaiSecurity
 	/**
 	 * @return is this user allowed to access Oncourse Dept Admin Tools?
 	 */
-	protected boolean isAdminToolsUser()
+   public boolean isAdminToolsUser(User)
 	{
 		return false;
 	}
